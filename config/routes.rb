f3db654bd9f2236eac94e438762ea3c2f1f05f70
Josh Cohen <jacohen10@gmail.com>
2015-03-12 22:00:00 -0400

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get "about" => "welcome#about"
+  get "blog" => "welcome#blog"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
