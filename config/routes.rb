cdba5a9e315f650b8e254bfe704e4203a69529a3
Josh Cohen <jacohen10@gmail.com>
2015-03-14 21:38:50 -0400

    added basic blog functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :articles do
+    resources :comments
+  end
+
   root 'welcome#index'
   get "about" => "welcome#about"
-  get "blog" => "welcome#blog"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
